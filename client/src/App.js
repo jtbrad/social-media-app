1f37fd98db0049454b8f1353ba44fccf2b45df2f
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-12-17 14:16:37 -0700

    added placeholder for user registration page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch, } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import NoMatch from './components/NoMatch';
+import Register from './components/Register';
 
 const App = () => (
   <>
   <Navbar />
   <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/register" component={Register} />
       <Route component={NoMatch} /> 
   </Switch>
   </>
---
+++ client/src/components/Register.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Header, } from 'semantic-ui-react';
+
+const Register = () => (
+  <Header as="h1">Register</Header>
+);
+
+export default Register;
